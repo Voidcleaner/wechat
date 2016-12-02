9e98ad5ee2b8d8408a784359fa05f525f15ef35b
Bao <brady.bao@viasystems.pri>
2016-12-02 16:52:22 +0800

    add function post

--- core/server/wxauthorize.py
+++ core/server/wxauthorize.py
@@ -1,6 +1,13 @@
 from core.logger_helper import logger
 import hashlib
 import tornado.web
+import time
+import urllib
+from urllib import parse
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
 
 
 class WxSignatureHandler(tornado.web.RequestHandler):
@@ -39,3 +46,28 @@ class WxSignatureHandler(tornado.web.RequestHandler):
         sha1 = hashlib.sha1(s.encode('utf-8')).hexdigest()
         logger.debug('sha1=' + sha1 + '&signature=' + signature)
         return sha1 == signature
+
+    def post(self):
+        body = self.request.body
+        logger.debug('微信消息回复中心收到用户消息' + str(body.decode('utf-8')))
+        data = ET.fromstring(body)
+        ToUserName = data.find('ToUserName').text
+        FromUserName = data.find('FromUserName').text
+        MsgType = data.find('MsgType').text
+        if MsgType == 'event':
+            '''接收事件推送'''
+            try:
+                Event = data.find('Event').text
+                if Event == 'subscribe':
+                    # 关注事件
+                    CreateTime = int(time.time())
+                    reply_content = '欢迎关注我们的公众号~'
+                    out = self.reply_text(FromUserName, ToUserName, CreateTime, reply_content)
+                    self.write(out)
+            except:
+                pass
+    def reply_text(self, FromUserName, ToUserName, CreateTime, Content):
+        '''回复文本消息模板'''
+        textTpl = """<xml> <ToUserName><![CDATA[%s]]></ToUserName> <FromUserName><![CDATA[%s]]></FromUserName> <CreateTime>%s</CreateTime> <MsgType><![CDATA[%s]]></MsgType> <Content><![CDATA[%s]]></Content></xml>"""
+        out = textTpl % (FromUserName, ToUserName, CreateTime, 'text', Content)
+        return out
