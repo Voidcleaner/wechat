4ea44d084c997139a58fd6f4fbdcf30cf8c7c97a
Bao <brady.bao@viasystems.pri>
2016-12-12 14:51:26 +0800

    fix bug

--- core/server/wxauthorize.py
+++ core/server/wxauthorize.py
@@ -146,6 +146,7 @@ class WxSignatureHandler(tornado.web.RequestHandler):
     def reply_text(self, FromUserName, ToUserName, CreateTime,MsgType, Content):
         """回复文本消息模板"""
         out = self.render_string('wechatpost.xml',ToUserName=ToUserName,FromUserName=FromUserName, CreateTime=CreateTime,MsgType=MsgType)
+        logger.debug(out)
         return out
 
     def check_signature(self, signature, timestamp, nonce):
