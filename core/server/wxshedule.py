1835b934bbb70fda77fe95a9ebef1b63a8bb1792
Bao <brady.bao@viasystems.pri>
2016-12-16 13:57:20 +0800

    add schedule get menu

--- core/server/wxshedule.py
+++ core/server/wxshedule.py
@@ -4,6 +4,7 @@ import requests
 import json
 from core.server.wxconfig import WxConfig
 from core.cache.tokencache import TokenCache
+from core.server.wxmenu import WxMenuServer
 
 
 class WxShedule(object):
@@ -16,19 +17,26 @@ class WxShedule(object):
     """
     _token_cache = TokenCache()  # 微信token缓存实例
     _expire_time_access_token = 7000 * 1000  # token过期时间
+    _check_menu_period = 24 * 60 * 60
 
     def excute(self):
         """执行定时器任务"""
         logger.info('【获取微信全局唯一票据access_token】>>>执行定时器任务')
         tornado.ioloop.IOLoop.instance().call_later(0, self.get_access_token)
-        tornado.ioloop.PeriodicCallback(self.get_access_token, self._expire_time_access_token).start()
+        tornado.ioloop.PeriodicCallback(
+            self.get_access_token, self._expire_time_access_token).start()
         # tornado.ioloop.IOLoop.current().start()
+
+        tornado.ioloop.IOLoop.instance().call_later(0, self.get_menu)
+        tornado.ioloop.PeriodicCallback(
+            self.get_menu, self._check_menu_period).start()
 
     def get_access_token(self):
         """获取微信全局唯一票据access_token"""
         url = WxConfig.config_get_access_token_url
         r = requests.get(url)
-        logger.info('【获取微信全局唯一票据access_token】Response[' + str(r.status_code) + ']')
+        logger.info('【获取微信全局唯一票据access_token】Response[' +
+                    str(r.status_code) + ']')
         if r.status_code == 200:
             res = r.text
             logger.info('【获取微信全局唯一票据access_token】>>>' + res)
@@ -36,22 +44,29 @@ class WxShedule(object):
             if 'access_token' in d.keys():
                 access_token = d['access_token']
                 # 添加至redis中
-                self._token_cache.set_access_cache(self._token_cache.KEY_ACCESS_TOKEN, access_token)
+                self._token_cache.set_access_cache(
+                    self._token_cache.KEY_ACCESS_TOKEN, access_token)
                 # 获取JS_SDK权限签名的jsapi_ticket
                 self.get_jsapi_ticket()
                 return access_token
             elif 'errcode' in d.keys():
                 errcode = d['errcode']
                 logger.info(
-                    '【获取微信全局唯一票据access_token-SDK】errcode[' + errcode + '] , will retry get_access_token() method after 10s')
-                tornado.ioloop.IOLoop.instance().call_later(10, self.get_access_token)
+                    '【获取微信全局唯一票据access_token-SDK】errcode[' +
+                    errcode +
+                    '] , will retry get_access_token() method after 10s')
+                tornado.ioloop.IOLoop.instance().call_later(
+                    10, self.get_access_token)
         else:
-            logger.error('【获取微信全局唯一票据access_token】request access_token error, will retry get_access_token() method after 10s')
-            tornado.ioloop.IOLoop.instance().call_later(10, self.get_access_token)
+            logger.error('【获取微信全局唯一票据access_token】request access_token error' +
+                         ',will retry get_access_token() method after 10s')
+            tornado.ioloop.IOLoop.instance().call_later(
+                10, self.get_access_token)
 
     def get_jsapi_ticket(self):
         """获取JS_SDK权限签名的jsapi_ticket"""
-        access_token = self._token_cache.get_cache(self._token_cache.KEY_ACCESS_TOKEN)
+        access_token = self._token_cache.get_cache(
+            self._token_cache.KEY_ACCESS_TOKEN)
         if access_token:
             url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=%s&type=jsapi' % access_token
             r = requests.get(url)
@@ -75,6 +90,23 @@ class WxShedule(object):
         else:
             logger.error('【微信JS-SDK】获取JS_SDK权限签名的jsapi_ticket时,access_token获取失败, will retry get_access_token() method after 10s')
             tornado.ioloop.IOLoop.instance().call_later(10, self.get_access_token)
+
+    def get_menu(self):
+        """获取menu,如果没有menu则进行创建"""
+        menu = WxMenuServer()
+
+        try:
+
+            data = menu.get_menu()
+            logger.debug('微信获取menu' + data)
+            if data == 0:
+                pass
+            else:
+                menu.create_menu()
+        except Exception as e:
+
+            logger.error('Exception:' + e)
+
 
 if __name__ == '__main__':
 
